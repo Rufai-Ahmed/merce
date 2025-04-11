fad49464e43339e93bdf5c6180db64b7d26dc5eb
Rufai-Ahmed <abbeyrufai234@gmail.com>
2025-04-11 07:32:46 +0100

    fix: image host

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { remotePatterns: [{ protocol: "https", hostname: "bigvybz.com" }] },
+  images: { remotePatterns: [{ protocol: "https", hostname: "api.bigvybz.com" }] },
 };
 
 export default nextConfig;
